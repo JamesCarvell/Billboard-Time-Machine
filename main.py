86dc35c22fc3d6c577ccacc4cf0e7290b4d573a7
JamesCarvell <103058258+JamesCarvell@users.noreply.github.com>
2023-01-11 20:39:31 -0400

    update main.py
    main.py now tries to find songs from Billboard 100 on Spotify, creates a playlist out of the songs it finds, and returns the url to the user.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 
 which_year = input("which year do you want to travel to? Type the date in this format YYYY-MM-DD:")
 past_billboard = f"https://www.billboard.com/charts/hot-100/{which_year}"
@@ -11,23 +11,26 @@ soup = BeautifulSoup(response.text, "html.parser")
 songs = []
 list_song_elements = soup.find_all(class_="o-chart-results-list-row")
 for element in list_song_elements:
-    rank_element = element.find(class_="c-label")
-    rank = rank_element.getText().strip()
     title_element = element.find(class_="c-title")
     title = title_element.getText().strip()
     label_element = element.select_one("span.c-label.lrv-u-display-block")
     label = label_element.getText().strip()
-    songs.append((rank, title, label))
+    songs.append((title, label))
 
-print(songs)
+scope = "playlist-modify-private playlist-read-private user-read-recently-played"
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
-auth_manager = SpotifyClientCredentials()
-sp = spotipy.Spotify(auth_manager=auth_manager)
+songs_URIs = []
+for song in songs:
+    track_dict = sp.search(q=f"track:{song[0]} artist:{song[1]}", type="track")
+    track_items = track_dict["tracks"]["items"]
+    if not track_items: continue
+    track_URI = track_items[0]['uri']
+    songs_URIs.append(track_URI)
 
-songs_URI = []
+playlist = sp.user_playlist_create(user=sp.current_user()["id"], name=f"{which_year} Billboard 100", public=False)
+playlist_id = playlist["id"]
+sp.playlist_add_items(playlist_id=playlist_id, items=songs_URIs)
+print(f"Your playlist can be found at {playlist['external_urls']['spotify']}")
 
-# TODO 3-1: Using the Spotipy documentation, create a list of Spotify song URIs for the list of song names you
-# TODO 3-1 ctd: found from step 1 (scraping billboard 100).
-# TODO 4-1: Using the Spotipy documentation, create a new private playlist with the name "YYYY-MM-DD Billboard 100",
-# TODO 4-1 ctd: where the date is the date you inputted in step 1.
-# TODO 4-2: Add each of the songs found in Step 3 to the new playlist.
+# TODO: comments
